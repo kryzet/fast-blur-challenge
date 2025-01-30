1915df1c0812017e9e4d7751f3ba6234a748eee1
Hassan Khalloof <166802607+kryzet@users.noreply.github.com>
2025-01-30 22:09:48 +0400

    optimization attempt #2
    Guided by ChatGPT-dissociate horizontal and vertical summations into separable kernels, which reduces the time complexity of my original naive solution by n^2. The other aspect that now causes this attempt to fail is that I don't understand how to implement a sliding window on top of that, which ChatGPT suggested as well and wrote code for that I could not ~~comprehend~~ grok ;)

--- blur.c
+++ blur.c
@@ -1,25 +1,41 @@
 #include "filter.h"
-#include <stdlib.h>
+#include <math.h>
 
 void blur(uint32_t height, uint32_t width, RGBTRIPLE input[height][width],
-    RGBTRIPLE output[height][width], uint32_t size)
+          RGBTRIPLE output[height][width], uint32_t size)
 {
     const uint32_t BOX_LENGTH = size * 2 + 1;
 
-    /* Optimization attempt 1 */
+    // Used for indexing into sums[]
+    enum { RED, GREEN, BLUE };
 
-    RGBTRIPLE **average_grid = calloc(2 * (BOX_LENGTH), sizeof(RGBTRIPLE));
+    /* Optimization attempt 2 */
+    // ========================================
+    RGBTRIPLE row_sum[BOX_LENGTH];
+    for (uint32_t i = 0; i < BOX_LENGTH; ++i)
+        row_sum[i] = input[0][i];
+    // ========================================
+
     for (uint32_t i = 0; i < height; ++i)
         for (uint32_t j = 0; j < width; ++j) {
-            uint64_t n_subtractions = 0, n_additions = 0;
+            int sums[3] = {0};
+            float counter = 0;
+            // Obtain needed pixels from copy[][]
             for (uint32_t x = i < size ? 0 : i - size,
-                          last_x = i + size >= height ? height : i + size;
+                          last_x = i + size >= height ? width : i + size;
                  x <= last_x; ++x)
                 for (uint32_t y = j < size ? 0 : j - size,
                               last_y = j + size >= width ? width : j + size;
                      y <= last_y; ++y) {
+                    sums[RED] += input[x][y].rgbtRed;
+                    sums[GREEN] += input[x][y].rgbtGreen;
+                    sums[BLUE] += input[x][y].rgbtBlue;
+                    ++counter;
                 }
+            for (int k = 0; k < 3; ++k)
+                sums[k] = fmin(round(sums[k] / counter), UINT8_MAX);
+            output[i][j].rgbtRed = sums[RED];
+            output[i][j].rgbtGreen = sums[GREEN];
+            output[i][j].rgbtBlue = sums[BLUE];
         }
-
 }
-
